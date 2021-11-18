00b7a35d3566f4b6f75380c910c4782a11b5d600
Caleb-Tay <93315108+Caleb-Tay@users.noreply.github.com>
2021-11-18 16:36:11 +0800

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,31 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    
+    this.cannonImg = loadImage("assets/canon.png");
+    this.cannonBaseImg = loadImage("assets/cannonBase.png");
+  }
+  
+  display(){
+    push();
+
+    translate(this.x,this.y);
+    rotate(this.angle);
+
+    imageMode(CENTER);
+    image(this.cannonImg, 0 , 0 , this.width, this.height);
+
+    pop();
+
+
+
+    // imageMode(CENTER);
+    image(this.cannonBaseImg,70,35,200,200);
+
+    
+  }
+}
